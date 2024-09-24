6e4276dd5316c3275e6b6ff665ac644bf1ed675b
Gerald Dukuly <gerald101201@gmail.com>
2024-09-23 20:18:46 -0400

    moved files to new folder

---
+++ server/RestaurantAPI/Models/Reservation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RestaurantAPI.Models;
+
+public partial class Reservation
+{
+    public int ReservationId { get; set; }
+
+    public int? TableId { get; set; }
+
+    public string CustomerName { get; set; } = null!;
+
+    public string CustomerEmail { get; set; } = null!;
+
+    public string? CustomerPhone { get; set; }
+
+    public int PartySize { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public TimeOnly Time { get; set; }
+
+    public string? SpecialRequests { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual Table? Table { get; set; }
+}
---
+++ server/RestaurantAPI/Models/Table.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace RestaurantAPI.Models;
+
+public partial class Table
+{
+    public int TableId { get; set; }
+
+    public string TableNumber { get; set; } = null!;
+
+    public int Capacity { get; set; }
+
+    public bool? IsAvailable { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+}
