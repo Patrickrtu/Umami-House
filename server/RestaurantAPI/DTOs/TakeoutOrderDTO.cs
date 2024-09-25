eaee744d6d6a713f8ba83d857837138206aff9f3
Gerald Dukuly <gerald101201@gmail.com>
2024-09-25 16:02:00 -0400

    Added TakeoutOrder DTO

---
+++ server/RestaurantAPI/DTOs/TakeoutOrderDTO.cs
@@ -0,0 +1,18 @@
+﻿using RestaurantAPI.Models;
+
+namespace RestaurantAPI.DTOs
+{
+    public class TakeoutOrderDTO
+    {
+        public int OrderId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public DateTime PickupTime { get; set; }
+
+        public string Status { get; set; } = null!;
+        public decimal TotalAmount { get; set; }
+
+        public virtual ICollection<OrderItemsDTO> OrderItems { get; set; } = new List<OrderItemsDTO>();
+    }
+}
