6be9221b3aaa1ea8656e8372030ac5c574e35f85
Favour Salako <salakofavour0@gmail.com>
2024-09-27 16:39:14 -0400

    some changes

--- server/RestaurantAPI/obj/Debug/net8.0/RestaurantAPI.AssemblyInfo.cs
+++ server/RestaurantAPI/obj/Debug/net8.0/RestaurantAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa545e2e6cba9de3e0ec798391b6d74c39ec03ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e37b034b6066fa2ab80d01141e1142e57429932")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
