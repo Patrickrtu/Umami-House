e28f33b491f3390a0701646afff2036e2aa8a286
Patrick Tung <83930287+Patrickrtu@users.noreply.github.com>
2024-09-30 15:39:35 -0500

    chore: updated AboutUS

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,71 +1,84 @@
-import SalakoFavour from '../assets/SalakoFavour.jpg';
-import '../css/styles.css';
+import SalakoFavour from "../assets/SalakoFavour.jpg";
+import Patrick from "../assets/patrick.jpg";
+import "../css/styles.css";
 
 const person = {
-    Gerald:{
-        description:"",
-        picture: "",
-    },
-    Muhammad:{
-        description:"",
-        picture: "",
-    },
-    Patrick:{
-        description:"",
-        picture: "",
-    },
-    Nicholas:{
-        description:"",
-        picture: "",
-    },
-    Micah:{
-        description:"",
-        picture: "",
-    },
-    Favour:{
-        description:"Favour is one of the founders of this Culinary Masterpiece alongside 4 friends. Favour has a lot of recognitions and accolades but he would always say - The journey is more important than the destination",
-        picture: SalakoFavour,
-    }
+  Gerald: {
+    description: "",
+    picture: "",
+  },
+  Muhammad: {
+    description: "",
+    picture: "",
+  },
+  Patrick: {
+    description:
+      "Patrick is focused on React.js and SQL development. He's passionate about creating seamless user experiences.",
+    picture: Patrick,
+  },
+  Nicholas: {
+    description: "",
+    picture: "",
+  },
+  Micah: {
+    description: "",
+    picture: "",
+  },
+  Favour: {
+    description:
+      "Favour is one of the founders of this Culinary Masterpiece alongside 4 friends. Favour has a lot of recognitions and accolades but he would always say - The journey is more important than the destination",
+    picture: SalakoFavour,
+  },
+};
 
-}
+const Cards = ({ image, desc }) => {
+  return (
+    <div className="containerCard">
+      <div className="inner-imageCard">
+        <img src={image} alt="A founders Picture" className="fab2" />
+      </div>
+      <div className="inner-textCard">
+        <h4>{desc}</h4>
+      </div>
+    </div>
+  );
+};
 
+function AboutUs() {
+  return (
+    <div>
+      <h1 className="page-header">Inspiration</h1>
 
-const Cards = ({image, desc})=>{
-    return(
-        <div className="containerCard">
-            <div className="inner-imageCard">
-                <img src={image} alt="A founders Picture" className='fab2' />
-            </div>
-            <div className="inner-textCard">
-                <h4>{desc}</h4>
-            </div>
-        </div>
+      <h3 className="page-section">
+        {" "}
+        Umami house is the culmination of passion and experience from 5 friends.
+        It traces its root to a shared love and appreciation for Japanese
+        Cuisine as well as business in general. Our Founders want to share the
+        beauty of Japanese Cuisine with the world and believe everyone would
+        come to understand as soon as you have a taste of the rich flavours and
+        textures of the meals at Umami House.
+      </h3>
 
-    );
-}
-
-function AboutUs(){
-    return(
-        <div>
-            <h1 className='page-header'>Inspiration</h1>
-
-            <h3 className="page-section"> Umami house is the culmination of passion and experience from 5 friends. It traces its root to a shared love and appreciation for Japanese Cuisine as well as business in general. Our Founders want to share the beauty of Japanese Cuisine with the world and believe everyone would come to understand as soon as you have a taste of the rich flavours and textures of the meals at Umami House.</h3>
-   
-            <h1 className='page-header'>Founders</h1>
-            <div className='container'>
-
-                <Cards image={person.Favour.picture} desc={person.Favour.description} />
-                <Cards image={person.Muhammad.picture} desc={person.Muhammad.description} />
-                <Cards image={person.Gerald.picture} desc={person.Gerald.description} />
-                <Cards image={person.Patrick.picture} desc={person.Patrick.description} />
-                <Cards image={person.Micah.picture} desc={person.Micah.description} />
-                <Cards image={person.Nicholas.picture} desc={person.Nicholas.description} />
-
-            </div>
-        </div>
-
-
-    )
+      <h1 className="page-header">Founders</h1>
+      <div className="container">
+        <Cards image={person.Favour.picture} desc={person.Favour.description} />
+        <Cards
+          image={person.Muhammad.picture}
+          desc={person.Muhammad.description}
+        />
+        <Cards image={person.Gerald.picture} desc={person.Gerald.description} />
+        <Cards
+          image={person.Patrick.picture}
+          desc={person.Patrick.description}
+        />
+        <Cards image={person.Micah.picture} desc={person.Micah.description} />
+        <Cards
+          image={person.Nicholas.picture}
+          desc={person.Nicholas.description}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default AboutUs;
