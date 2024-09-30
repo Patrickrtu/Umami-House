fd22f780fee5545db8e85b527f875dc156f12c4c
Nickybox <naudette@umass.edu>
2024-09-30 12:42:14 -0400

    Added 'About' and 'Connect' to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,9 +24,11 @@ function Header() {
         <h1 className="logo">Umami House</h1>
         <ul className="nav-links">
           <li><Link to="/" className="nav-link">Home</Link></li>
+          <li><Link to="/aboutus" className="nav-link">About</Link></li>
           <li><Link to="/menu" className="nav-link">Menu</Link></li>
           <li><Link to="/reservations" className="nav-link">Reservations</Link></li>
           <li><Link to="/takeout" className="nav-link">Takeout</Link></li>
+          <li><Link to="/connect" className="nav-link">Connect</Link></li>
         </ul>
       </nav>
     </header>
